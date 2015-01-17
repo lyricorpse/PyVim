950994c49a5e967a7e5dfa5ab674febf51846b88
Lyricorpse <lyricorpse@gmail.com>
2015-01-17 16:27:12 -0600

    fix a bug in ncl autocompletion

--- pyvim.py
+++ pyvim.py
@@ -100,7 +100,7 @@ configs = {
     set complete-=k complete+=k
     set wildmode=list:full
     set wildmenu
-    au BufRead,BufNewFile *ncl set dictionary=~/.vim/dictionary/ncl.dic.
+    au BufRead,BufNewFile *ncl set dictionary=~/.vim/dictionary/ncl.dic
     ''',
 }
 
